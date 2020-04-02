21b28f40928da3d7bc9231897a881bb3cd2160bc
Volodiax <volodiax@icloud.com>
2020-04-02 20:33:21 +0300

    start navigation controller to table view controller

---
+++ KafeShowSwift5/MainTableViewController.swift
@@ -0,0 +1,52 @@
+//
+//  MainTableViewController.swift
+//  KafeShowSwift5
+//
+//  Created by v on 02.04.2020.
+//  Copyright © 2020 volodiax. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 4
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CellRestaurant", for: indexPath)
+
+        cell.textLabel?.text = "some text \(indexPath.row)"
+
+        return cell
+    }
+ 
+
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
